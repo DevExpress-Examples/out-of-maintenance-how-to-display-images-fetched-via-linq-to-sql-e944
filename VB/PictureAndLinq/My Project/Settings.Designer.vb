73827922ed2502f29a73f1264ba3db33110129c7
Alexey Zelenov (DevExpress) <alexey.zelenov@devexpress.com>
2020-12-03 17:33:47 +0300

    update example

--- VB/PictureAndLinq/My Project/Settings.Designer.vb
+++ VB/PictureAndLinq/My Project/Settings.Designer.vb
@@ -14,7 +14,7 @@ Option Explicit On
 
 
 <Global.System.Runtime.CompilerServices.CompilerGeneratedAttribute(),  _
- Global.System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.9.0.0"),  _
+ Global.System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.4.0.0"),  _
  Global.System.ComponentModel.EditorBrowsableAttribute(Global.System.ComponentModel.EditorBrowsableState.Advanced)>  _
 Partial Friend NotInheritable Class Settings
     Inherits Global.System.Configuration.ApplicationSettingsBase
@@ -52,11 +52,11 @@ Partial Friend NotInheritable Class Settings
             Return defaultInstance
         End Get
     End Property
-
-    <Global.System.Configuration.ApplicationScopedSettingAttribute(),
-     Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),
-     Global.System.Configuration.SpecialSettingAttribute(Global.System.Configuration.SpecialSetting.ConnectionString),
-     Global.System.Configuration.DefaultSettingValueAttribute("Data Source=(local);Initial Catalog=AdventureWorks2012;Integrated Security=True")>
+    
+    <Global.System.Configuration.ApplicationScopedSettingAttribute(),  _
+     Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
+     Global.System.Configuration.SpecialSettingAttribute(Global.System.Configuration.SpecialSetting.ConnectionString),  _
+     Global.System.Configuration.DefaultSettingValueAttribute("Data Source=(local);Initial Catalog=AdventureWorks;Integrated Security=True")>  _
     Public ReadOnly Property AdventureWorksConnectionString() As String
         Get
             Return CType(Me("AdventureWorksConnectionString"),String)
